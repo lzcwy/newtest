369a9dad30e16f4ebe54c0746cbb88ff8e243ac3
lzcwy <qqsvip666@163.com>
2023-07-20 21:50:46 +0800

    fix

--- porn.py
+++ porn.py
@@ -25,15 +25,15 @@ class Porn(Plugin):
     def help(self, **kwargs) -> str:
         return "Use the command #porn(or whatever you like set with command field in the config) to get a wonderful video"
 
-    def encrypt(raw):
+    def encrypt(self,raw):
         raw_str = json.dumps(raw)
-        raw_bytes = pad(raw_str.encode(), 16)
-        cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
-        return base64.b64encode(cipher.encrypt(raw_bytes))
+        raw_bytes = pad(raw_str.encode('utf-8'), 16)
+        cipher = AES.new(self.key.encode('utf-8'), AES.MODE_ECB)
+        return base64.b64encode(cipher.encrypt(raw_bytes)).decode('utf-8') 
 
-    def decrypt(enc):
+    def decrypt(self,enc):
         enc = base64.b64decode(enc)
-        cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
+        cipher = AES.new(self.key.encode('utf-8'), AES.MODE_ECB)
         plaintext = unpad(cipher.decrypt(enc), 16)
         plaintext = plaintext.decode('utf-8')
         return json.loads(plaintext)
@@ -44,20 +44,22 @@ class Porn(Plugin):
             header = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
             }
-            params = {'order_key': '', 'tag_id': '', 'is_long': 0, 'paytype': '', 'keywords': '', 'type_id': '', 'code': '', 'page': 1, 'page_size': 20}
-            payload=json.dumps(params)
-            payload = self.encrypt(payload)
+            random_page = random.randint(1, 20)
+            params = {'order_key': '', 'tag_id': '', 'is_long': 0, 'paytype': '', 'keywords': '', 'type_id': '', 'code': '', 'page': random_page, 'page_size': 20}
+            payload = self.encrypt(params)
+            payload = {
+                'data': payload
+            }
             response = requests.post(
                 "https://c.onljx.cc/h5/app/api/video/search", data=payload,headers = header
             ).json()
-            print(response)
             if response['code'] == 200:
 
                 choiceData = self.decrypt(response['data'])
                 choiceItem = random.choice(choiceData)
 
                 videos_url = choiceItem['smu']
-                print(videos_url)
+                # print(videos_url)
                 if len(videos_url) > 0:
                     reply = Reply(ReplyType.M3U8, f"{videos_url}")
                 else:
